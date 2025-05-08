113db156bc0ca4a57edef4e684a7d63e220f85b1
RevivalistHeroes <146007105+RevivalistHeroes@users.noreply.github.com>
2025-05-08 19:07:56 +0700

    Update Turnover_Website.py

--- Turnover_Website.py
+++ Turnover_Website.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import sklearn
 
 # Konfigurasi halaman
 st.set_page_config(page_title="Aplikasi Prediksi", layout="wide")
