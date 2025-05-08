a9cc66f0f292f72cb1b7bd8415f33c2fcbbcfb83
RevivalistHeroes <146007105+RevivalistHeroes@users.noreply.github.com>
2025-05-08 18:59:44 +0700

    Update Turnover_Website.py

--- Turnover_Website.py
+++ Turnover_Website.py
@@ -12,7 +12,7 @@ st.title("Prediksi Otomatis dari Dataset")
 # Load model statis dari file
 MODEL_PATH = "model.pkl"
 with open(MODEL_PATH, "rb") as f:
-    model = pickle.load(f)
+    pickle.dump(MODEL_PATH, model_file)
 
 st.subheader("Formulir Input Manual untuk Prediksi")
 
